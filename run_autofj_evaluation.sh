51881814c25212ca920d90a42a17ba4d660987e2
Grace Fan <gf2467@cm012.hpc.nyu.edu>
2026-01-28 22:13:19 -0500

    delete old scripts

--- run_autofj_evaluation.sh
+++
@@ -1,9 +0,0 @@
-# AutoFJ-style: fix precision targets and report the best recall you can get
-# (maximize recall s.t. precision >= target), using retrieval-evidence matches.
-time python evaluate_autofj_experiment.py \
-    --semantic-results autofj_query_results_k1024_t0.1_top10_slurm/all_query_results.csv \
-    --output-dir autofj_evaluation_results \
-    --use-contributing-entities \
-    --precision-targets 0.7 0.8 0.9
-
-#     --skip-entity-linking # only joinability
